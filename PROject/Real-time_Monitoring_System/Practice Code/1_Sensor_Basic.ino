49f3ffff576a5a5134ab3298b6ea73109820d1ee
Yoonho Kim <kyhy0321@gmail.com>
2021-12-18 16:41:46 +0000

    add test code

---
+++ PROject/Real-time_Monitoring_System/Practice Code/1_Sensor_Basic.ino
@@ -0,0 +1,40 @@
+// 목적: 시리얼 통신을 이해하고 있는지 확인하는 코드
+// 수행할 항목 : - 처리되어 있는 줄의 코드를 작성
+
+// 전제 조건: 
+// DHT22 센서를 사용, digital pin 4번 사용
+// 컴퓨터 to 아두이노, 유선 연결
+
+/*******************************************/
+//DHT22 설정
+#include "DHT.h" //DHT22 헤더 호출
+
+#define DHTPIN 4 //DHT22 정보 수신 핀 지정
+#define DHTTYPE DHT22 //DHT 센서들 중, DHT22를 사용
+DHT dht(DHTPIN, DHTTYPE); //DHT센서 선언, DHT핀은 4번, DHT타입은 DHT22
+
+String humidity; //DHT센서, 습도를 문자열로 저장하는 변수
+String temperture; //DHT센서, 온도를 문자열로 저장하는 변수
+
+/*******************************************/
+//setup
+void setup() {
+  - //시리얼 통신 시작, 9600 비트레이트로 설정 | 유선 연결, 컴퓨터 to 아두이노
+  dht.begin(); //DHT22센서 시작
+}
+
+/*******************************************/
+//loop()
+void loop() {
+  float h = dht.readHumidity();  //변수 f에 습도 값을 저장 | DHT.h에 있는 함수입니다.
+  float t = dht.readTemperature(); //변수 t에 온도 값을 저장 | DHT.h에 있는 함수입니다.
+  
+  humidity = String(h); // 습도값을 문자열로 전환 | float -> string
+  temperture = String(t); // 온도값을 문자열로 전환 | float -> string
+
+  //"습도값[문자열], 온도값[문자열]"로 송출됨. | 이유는 파이썬에서, 콤마(,)를 기준으로 값을 나눌 것이기 때문.
+
+  - // 시리얼 통신 | 습도값 송신 | ?.print("내용")은 "내용"을 상대 시리얼 모듈로 송신하는 것
+  - // 시리얼 통신 | 온도값 송신 | ?.println("내용")은 "내용"을 상대 시리얼 모듈로 송신하는 것
+
+}
---
+++ PROject/Real-time_Monitoring_System/Practice Code/3_Serial2Python.py
@@ -0,0 +1,23 @@
+# 목적: 파이썬 to 아두이노의 시리얼 통신을 이해하고 있는지 확인하는 코드
+# 수행할 항목 :
+# 1. - 처리되어 있는 줄의 코드를 작성
+# 2. 모든 코드의 주석을 추가
+
+# 전제 조건: 
+# 컴퓨터 to 아두이노, 근거리 무선 연결
+# 아두이노는 블루투스로 연결되었고, 문자열을 지속적으로 출력한다.
+# 아두이노의 블루투스 통신 포트는 COM8이다.
+
+import serial # 파이썬에서 시리얼 통신을 하기 위해, 호출한 라이브러리
+
+serial_sensor = -.Serial( - , 9600) # 
+
+def sensor_input():
+    if serial_sensor.readable():  # 시리얼 통신이 이루어질 때, ser.readable()의 값이 1이 된다. 그러므로, if문이 실행됨.
+        val = serial_sensor.readline()  #
+        val = val.decode('utf-8')[:len(val) - 2] #
+        print(val) # 사용자에게 어떤 값을 표시하는지 간단히 설명하시오.
+
+if __name__ == '__main__':
+    while 1:
+         sensor_input()
