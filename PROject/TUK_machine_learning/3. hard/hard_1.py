4b138db46c5fb0df2e712400d26e62ece057af7e
Yoonho Kim <kyhy0321@gmail.com>
2022-06-28 15:46:29 +0000

    22.06.29 hard_1, 0<n<100 commit

--- PROject/TUK_machine_learning/3. hard/hard_1.py
+++ PROject/TUK_machine_learning/3. hard/hard_1.py
@@ -4,6 +4,7 @@
 
 def over10(num):
     numList = num
+    if int(numList) < 10: return "under10" # 22.06.29 | 10 < n < 100에서 0 < n < 100로 조건을 변경했다. 분기 추가
     total = int(numList[0]) + int(numList[1])
     if total >= 10: return True
     else: return False
@@ -15,7 +16,8 @@ def plusNum(num):
         numPlus0 = str(int(numList0[0]) + int(numList0[1]))[1]
         total =  numList0[1] + numPlus0
     elif over10(num) is False:
-        total = num[1] + str(int(numList0[0]) + int(numList0[1]))
+        total = numList0[1] + str(int(numList0[0]) + int(numList0[1]))
+    elif over10(num) == "under10": total = str(int(numList0)) + str(int(numList0)) # 22.06.29 | 0 < n < 100로 조건 변경. | 앞 문자 "0"을 int로 제거후, str으로 변환한다.
     return total
 
 ############################################################
@@ -27,11 +29,11 @@ repeat = 1
 error = ""
 
 while 1:
-    if 10 > int(fstNum) or int(fstNum) > 100:
+    if 0 > int(fstNum) or int(fstNum) > 100:
         error = "10"
         break
     saveNum = plusNum(saveNum)
-    if fstNum == saveNum:  break
+    if int(fstNum) == int(saveNum):  break
     repeat += 1
 
 
